0c90e5f2191174f77ec4d08ff9228e2b5612c43f
Daniela Chavez <danielachavez18@gmail.com>
2019-10-24 12:54:20 -0500

    thought we had to use handlebars for some reason so i removed it

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+// Dependencies
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var path = require('path');
  
 var app = express();
 
@@ -7,27 +8,9 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
-// Set Handlebars as the default templating engine.
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-
-// Data
-
-
-
-
-
-
-//Routes
-
-app.get("/weekday", function(req, res) {
-    res.render("index", lunches[0]);
-  });
-
-  
-
-
+// Sets up the Express App to handle data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 
 // Start our server so that it can begin listening to client requests.
@@ -35,4 +18,45 @@ app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
   });
+
+
+// Data
+
+var data = {
+    surveyQuestions:[]
+}
+
+var surveyQuestions = [
+    {
+      questionOne: "What is your favorite color?",
+      answer: true,
+      score: 4
+    }, {
+      questionTwo: "How many siblings do you have?",
+      answer: true,
+      score: 10
+    }, 
+  ];
+  
+
+for (var i = 0; i < surveyQuestions.length; i++) {
+    var data = surveyQuestions[index];
+    
+}
+
+
+//Routes
+
+app.get("/surveyQuestions", function(req, res) {
+    res.render("index", surveyQuestions.filter(item =>
+        item.surveyQuestions === true));
+  });
+  
+
+
+
+
+
+
+
   
